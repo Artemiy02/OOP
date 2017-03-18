5eb3473a2fc80df8cc0007deabceffece9883b67
Артемий <>
2017-03-18 20:13:30 +0200

    Работа с базой

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,6 +4,11 @@ class NewsController
 {
 		public function actionAll()
 			{
+				// $art = NewsModel::findOneByColumn('title', 'Новый заголовок1453');
+				// $art->title = 'Новый заголовок1';
+				// $art->save();
+				//
+				// var_dump($art->text);
 
 					$items = NewsModel::findAll();
 					$view = new View();
@@ -24,8 +29,8 @@ class NewsController
 		public function actionInsert()
 			{
 					$article = new NewsModel;
-					$article->title = 'Дарова!';
-					$article->text = 'Черьви!';
+					// $article->title = 'Дарова!';
+					// $article->text = 'Черьви!';
 					$article->insert();
 			}
 }
