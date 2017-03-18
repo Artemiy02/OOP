92ff6716a85013ef35162f12a56487f3682c0e61
Артемий <>
2017-03-18 20:14:16 +0200

    фф

--- classes/AbstractModel.php
+++ classes/AbstractModel.php
@@ -17,16 +17,28 @@ abstract class AbstractModel
 					return $this->data[$k];
 			}
 
+    public function __isset($k)
+      {
+        return isset($this->data[$k]);
+      }
 
-		public static function getTable()
-			{
-					return static::$table;
-			}
+    public static function findOneByColumn($column, $value)
+      {
+        $db = new DB;
+        $db->setClassName(get_called_class());
+        $sql = 'SELECT * FROM ' . static::$table . ' WHERE ' . $column . '=:value';
+        $res = $db->query($sql, [':value' => $value]);
+        if (!empty($res)){
+          return $res[0];
+        }
+        return false;
+      }
+
 
 		public static function findAll()
 			{
 					$class = get_called_class();
-					$sql = 'SELECT * FROM ' . static::getTable();
+					$sql = 'SELECT * FROM ' . static::$table;
 					$db = new DB;
 					$db->setClassName($class);
 					return $db->query($sql);
@@ -34,8 +46,10 @@ abstract class AbstractModel
 
 		public static function findOne($id)
 			{
-					$sql = 'SELECT * FROM ' . static::getTable() . ' WHERE id=:id';
+          $class = get_called_class();
+					$sql = 'SELECT * FROM ' . static::$table . ' WHERE id=:id';
 					$db = new DB();
+          $db->setClassName($class);
 					return $db->query($sql, [':id' => $id]);
 			}
 
@@ -55,5 +69,35 @@ abstract class AbstractModel
 					';
 					$db = new DB;
 					$db->execute($sql, $data);
+          $this->id = $db->lastInsertId();
 			}
+
+    public function update()
+      {
+          $cols = [];
+          $data = [];
+          foreach ($this->data as $k => $v) {
+            $data[':' . $k] = $v;
+            if ('id' == $k){
+              continue;
+            }
+            $cols[] = $k . '=:' . $k;
+          }
+            $sql = '
+              UPDATE ' . static::$table . '
+              SET ' . implode(', ', $cols) . '
+              WHERE id=:id
+          ';
+          $db = new DB;
+					$db->execute($sql, $data);
+      }
+
+    public function save()
+      {
+          if (!isset($this->id)){
+            $this->insert();
+          } else {
+            $this->update();
+          }
+      }
 }
