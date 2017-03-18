bfb96cba3a938863169a3dc445636019586dabf4
Артемий <>
2017-03-18 23:24:49 +0200

    +ЧПУ

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
 require_once __DIR__ . './autoload.php';
- $controllerName = isset($_GET['ctrl']) ? $_GET['ctrl'] . 'Controller' : 'NewsController';
- $action = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$pathParts = explode('/', $path);
+
+$ctrl = !empty($pathParts[2]) ? ucfirst($pathParts[2]) : 'News';
+$act = !empty($pathParts[3]) ? ucfirst($pathParts[3]) : 'All';
+
+$controllerName = $ctrl . 'Controller';
+
 
 try{
- $method = 'action'.$action;
+ $method = 'action'. $act;
  $controller = new $controllerName;
  $controller->$method();
 } catch (Exception $e) {
